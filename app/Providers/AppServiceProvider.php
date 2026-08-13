9fcda0faf7b061ace7288a2dd01f4c4e9802d5c6
Luthfi1995 <lutfihamid995@gmail.com>
2026-08-14 01:52:08 +0700

    fix: restore missing ServiceProvider import in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Carbon\Carbon;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
